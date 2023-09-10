d1d96c602df1907bff4bcd48705e6c3d2a1b8063
fairchildovo <48383458+fairchildovo@users.noreply.github.com>
2023-09-10 21:25:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git UA2F https://github.com/Zxilly/UA2F' >>feeds.conf.default
 git clone https://github.com/Zxilly/UA2F package/UA2F
+git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
