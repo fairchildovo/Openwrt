b8c83a4fe1288c5bb2246fd49933453621ae1981
fairchildovo <48383458+fairchildovo@users.noreply.github.com>
2023-09-11 11:00:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git UA2F https://github.com/Zxilly/UA2F' >>feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/Zxilly/UA2F package/UA2F
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
